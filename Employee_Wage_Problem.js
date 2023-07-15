9940f72a506f1c85228297a53e81f32282c005b6
Vedant <vedantnp200@gmail.com>
2023-07-15 17:02:11 +0530

    [Vedant]-Write function for daily working hours

--- Employee_Wage_Problem.js
+++ Employee_Wage_Problem.js
@@ -1,4 +1,4 @@
-// UC2 - Calculating employee wage
+// UC3 - Function to get work hours
 
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -6,20 +6,22 @@ const Part_Time_Hours = 4;
 const Full_Time_Hours = 8;
 const WAGE_PER_HOUR = 20;
 
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return Part_Time_Hours;
+        case IS_FULL_TIME:
+            return Full_Time_Hours;
+        default:
+            return 0;
+    }
+}
+
 let empHrs = 0;
 
 let empCheck = Math.floor(Math.random() * 10) % 3;
 
-switch (empCheck) {
-    case IS_PART_TIME:
-        empHrs = Part_Time_Hours;
-        break;
-    case IS_FULL_TIME:
-        empHrs = Full_Time_Hours;
-        break;
-    default:
-        empHrs = 0;
-}
+empHrs = getWorkingHours(empCheck);
 
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Emp Wage: " + empWage);
