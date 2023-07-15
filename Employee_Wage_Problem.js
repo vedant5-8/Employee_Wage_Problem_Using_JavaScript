8d9a386680a56d04feeb772c3c7f57b05f9a0d82
Vedant <vedantnp200@gmail.com>
2023-07-15 15:44:55 +0530

    [Vedant]-Calculating Employee Wage

--- Employee_Wage_Problem.js
+++ Employee_Wage_Problem.js
@@ -1,13 +1,25 @@
-// UC1 - Check employee presence
+// UC2 - Calculating employee wage
 
-const Is_Absent = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const Part_Time_Hours = 4;
+const Full_Time_Hours = 8;
+const WAGE_PER_HOUR = 20;
 
-let empCheck = Math.floor(Math.random() * 10) % 2;
+let empHrs = 0;
 
-if (empCheck == Is_Absent) {
-    console.log('Employee is absent.');
-    return;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+
+switch (empCheck) {
+    case IS_PART_TIME:
+        empHrs = Part_Time_Hours;
+        break;
+    case IS_FULL_TIME:
+        empHrs = Full_Time_Hours;
+        break;
+    default:
+        empHrs = 0;
 }
-else {
-    console.log('Employee is present.')
-}
+
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Emp Wage: " + empWage);
