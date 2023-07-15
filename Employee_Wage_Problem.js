1b3ec43e4de2e825aa75a317ebec62bdc5954dfc
Vedant <vedantnp200@gmail.com>
2023-07-15 19:38:01 +0530

    [Vedant]-Storing daily wage in a array

--- Employee_Wage_Problem.js
+++ Employee_Wage_Problem.js
@@ -1,5 +1,4 @@
-// UC5 - Calculating wages till a condition of total working hours of 160 or 
-// max days of 20 is reached for a month
+// UC6 - Storing daily wage in a array
 
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -18,20 +17,29 @@ function getWorkingHours(empCheck) {
     }
 }
 
+function calcDailyWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
+}
+
 const MAX_HRS_IN_MONTH = 160;
 const NUM_OF_WORKING_DAYS = 20;
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
 
 while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
     totalWorkingDays++;
     
     let empCheck = Math.floor(Math.random() * 10) % 3;
 
-    totalEmpHrs += getWorkingHours(empCheck);
+    let empHrs = getWorkingHours(empCheck);
+
+    totalEmpHrs += empHrs;
+
+    empDailyWageArr.push(calcDailyWage(empHrs));
 }
 
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
+let empWage = calcDailyWage(totalEmpHrs);
 
 console.log("Total Days: " + totalWorkingDays);
 console.log("Total Hrs: " + totalEmpHrs);
